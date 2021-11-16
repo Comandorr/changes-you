100ec37f683105e33ec0f9398504dfb15752266d
Comandorr <Plane135790@gmail.com>
2021-11-16 06:40:26 +0300

    следы от шин, чуть поменялась скорость тачки, оптимизирован дождь

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from random import*
 mixer.init()
 music = mixer.Channel(1)
 paint_it_black = mixer.Sound('sounds/paint.mp3')
+engine_start = mixer.Sound('sounds/engine_start.mp3')
 
 create_window(1920, 1080)
 from pyengine import win_w, win_h, center_x, center_y
+mouse.set_visible(False)
 world_x = center_x
 world_generated = []
 scene_car = []
 ground = []
 crates = []
+tires = []
 
 wind = []
 WIND = False
@@ -23,6 +26,7 @@ RAIN = False
 rain_rapid = 5
 rain_speed = 7
 
+ENGINE = True
 
 class WindDust(SimpleSprite):
     def __init__(self, x, y, size = (8, 4)):
@@ -77,18 +81,11 @@ class Car(SimpleSprite):
             self.fps = 0
 
     def up(self):
-        self.y -= self.speed
+        self.y -= self.speed/2
         self.aniframe = 5
 
     def down(self):
-        self.y += self.speed
-        self.aniframe = 5
-
-    def left(self):
-        global world_x
-        for i in scene_car:
-            i.x += self.speed
-        world_x -= self.speed
+        self.y += self.speed/2
         self.aniframe = 5
 
     def right(self):
@@ -97,11 +94,22 @@ class Car(SimpleSprite):
             i.x -= self.speed
         world_x += self.speed
         self.aniframe = 5
+        scene_car.append(SimpleSprite('images/black_square_50.png', self.x, self.y+14))
+        tires.append(scene_car[-1])
+        scene_car.append(SimpleSprite('images/black_square_50.png', self.x, self.y+20))
+        tires.append(scene_car[-1])
 
     def update(self):
         self.aniframe = 10
-        keyboard_control(self)
+        if ENGINE:
+            keys = key.get_pressed()
+            if keys[K_w]:
+                self.up()
+            if keys[K_s]:
+                self.down()    
+            self.right()
         self.animate()
+        
 
 
 car = Car('images/rounded_red.png', center_x/2, center_y, size=(62, 24), speed = 5)
@@ -124,8 +132,8 @@ text_up = SimpleText('год 2052', 64, win_w/2, win_h*0.1, color = white)
 text_up.position[0] = win_w/2 - text_up.rect.width/2
 text_down = SimpleText('ты последний выживший', 64, win_w/2, win_h*0.9, color = white)
 text_down.position[0] = win_w/2 - text_down.rect.width/2
-text_center = SimpleText('CHANGES', 64, win_w/2, win_h/2)
-text_center.position[0] = win_w/2 - text_down.rect.width/2
+text_center = SimpleText('CHANGES', 100, win_w/2, win_h/2)
+text_center.position[0] = win_w/2 - text_down.rect.width/1.7
 text_center.position[1] = win_h/2 - text_down.rect.height/2
 music.play(paint_it_black)
 start_time = time.get_ticks()
@@ -149,15 +157,10 @@ while cutscene:
         x = ground[-1].x
         for y in range(win_h//64+1):
             scene_car.append(SimpleSprite('images/mapTile_017.png', x+64, y*64))
-            ground.append(scene_car[-1])
-
-    
-    
-
-            
+            ground.append(scene_car[-1])  
 
     fill_window(black)
-    for g in ground:
+    for g in ground + tires:
         g.reset()
     car.animate()
     shadow.replace(car.x-3, car.y+10)
@@ -182,11 +185,15 @@ while cutscene:
     if time_passed > 7500:
         text_up.position[1] -= 3
         text_down.position[1] += 3
-    if time_passed > 11500:
+    if time_passed > 11550:
         text_center.reset()
     if time_passed > 8500:
         if car.x < center_x/4:
             car.x += 5
+            scene_car.append(SimpleSprite('images/black_square_50.png', car.x, car.y+14))
+            tires.append(scene_car[-1])
+            scene_car.append(SimpleSprite('images/black_square_50.png', car.x, car.y+20))
+            tires.append(scene_car[-1])
         else:
             car.right()
     if time_passed > 12500:
@@ -205,8 +212,7 @@ while cutscene:
     display.update()
     clock.tick(60)
 
-
-
+car.speed = 7
 run = True
 while run:
     for e in event.get():
@@ -217,6 +223,10 @@ while run:
                 WIND = not WIND
             if e.key == K_2:
                 RAIN = not RAIN
+            if e.key == K_d:
+                ENGINE = not ENGINE
+                if ENGINE:
+                    engine_start.play()
 
     fill_window(black)
     for i in ground.copy():
@@ -230,23 +240,34 @@ while run:
             scene_car.remove(i)
             crates.remove(i)
 
+    for i in tires.copy():
+        i.reset()
+        if i.x <= -64:
+            scene_car.remove(i)
+            tires.remove(i)
+
     if WIND:
         for y in range(0, win_h):
             if chance(wind_rapid, max = 1001):
                 scene_car.append(WindDust(x = win_w, y = y))
 
     if RAIN:
-        for x in range(0, win_w*2):
+        for x in range(0, int(win_w/2)):
             if chance(rain_rapid, max = 1001):
-                scene_car.append(RainDrop(x, y = 0))
+                scene_car.append(RainDrop(x*4, y = 0))
 
-    if ground[-1].x <= world_x + win_w/2:
+    if ground[-1].x <= win_w:
         x = ground[-1].x
         for y in range(win_h//64+1):
             scene_car.append(SimpleSprite('images/mapTile_017.png', x+64, y*64))
             ground.append(scene_car[-1])
-            if chance(5):
-                scene_car.append(SimpleSprite('images/crate_12.png', x+64, y*64, size = (32, 32)))
+            if chance(2):
+                img = choice(['barrel_red_down.png', 'tanks_barrelGreen.png', 'tanks_barrelGrey.png', 'tanks_tankDesert_body1.png', 'tanks_tankDesert_body3.png'])
+                scene_car.append(SimpleSprite('images/garbage/'+img, x+64, y*64))
+                crates.append(scene_car[-1])
+            if chance(2):
+                img = choice(['01', '02', '03', '04', '05', '16', '18', '31', '32', '33'])
+                scene_car.append(SimpleSprite('images/tiles/mapTile_0'+img+'.png', x+64, y*64))
                 crates.append(scene_car[-1])
     
     car.update()
